bd8ca98f16913b3b1bf92023dc03f412ba15f8b1
MrFuentastic <vfuentes8587@gmail.com>
2017-10-22 13:02:58 -0500

    added search filters and sorting buttons

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,7 +7,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
       newVbVehicle: "",
       newVbOrg: "",
       newVbQuote: "",
-      errors: []
+      errors: [],
+      searchFilter: "",
+      sortAttribute: "character",
+      sortAscending: true
     },
     mounted: function() {
       $.get('/api/v2/venture_bros.json', function(data) {
@@ -15,9 +18,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }.bind(this));
     },
     methods: {
-      toggleBio: function(currentDiv) {
-        console.log(currentDiv.document.querySelectorAll("hidden"));
-      },
+      // toggleQuote: function(currentDiv) {
+      //   console.log(currentDiv.document.querySelectorAll("hidden"));
+      // },
       addChar: function() {
         var params = {
           character: this.newVbChar,
@@ -36,10 +39,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }.bind(this)).fail(function(response) {
           this.errors = response.responseJSON.errors;
         }.bind(this));
+      },
+      validChar: function(inputChar) {
+        var nameFilter = inputChar.character.toLowerCase().indexOf(this.searchFilter.toLowerCase()) !== -1;
+        var quoteFilter = inputChar.quote.toLowerCase().indexOf(this.searchFilter.toLowerCase()) !== -1;
+
+        return nameFilter || quoteFilter;
+      },
+      setSort: function(inputAttr) {
+        if (inputAttr !== this.sortAttribute) {
+          this.sortAscending = true;
+        } else {
+          this.sortAscending = !this.sortAscending;
+        }
+        this.sortAttribute = inputAttr;
+        
+        //fix this!!!!!!
+        if (this.sortAscending) {
+          document.getElementById(inputAttr).innerHTML += ' ^';
+        } else {
+          document.getElementById(inputAttr).innerHTML += ' v';          
+        }
       }
     },
     computed: {
-
+      sortedCharacters: function() {
+        return this.ventureBros.sort(function(char1, char2) {
+          if (this.sortAscending) {
+            return char1[this.sortAttribute].localeCompare(char2[this.sortAttribute]);
+          } else {
+            return char2[this.sortAttribute].localeCompare(char1[this.sortAttribute]);
+          }
+        }.bind(this));
+      }
     }
   });
 });
